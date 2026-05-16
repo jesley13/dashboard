e49862662c9e58b33a0eaef1e7eb5b308b9cf787
jesley <jesley13@gmail.com>
2026-05-16 17:06:47 +0530

    delete unwated files

--- inspect.js
+++
@@ -1,9 +0,0 @@
-const XLSX = require('xlsx');
-const workbook = XLSX.readFile('d:/dashboard/2526.xlsx');
-console.log('Sheets:', workbook.SheetNames);
-for (const name of workbook.SheetNames) {
-  console.log('--- Sheet:', name, '---');
-  const sheet = workbook.Sheets[name];
-  const json = XLSX.utils.sheet_to_json(sheet, { defval: null });
-  console.log('First 2 rows:', JSON.stringify(json.slice(0, 2), null, 2));
-}
--- start.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo Starting Dashboard Server...
-start http://localhost:4173/
-node server.js
